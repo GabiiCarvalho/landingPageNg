59d2c5673b1fde4545ca4ecfa767f12bcdaeefde
GabiiCarvalho <gabi.05assis9@gmail.com>
2025-07-05 21:51:30 -0300

    ajuste

--- backend/routes/quoteRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const quoteController = require('../controllers/quoteController');
-
-router.post('/quote', quoteController.submitQuoteRequest);
-
-module.exports = router;
