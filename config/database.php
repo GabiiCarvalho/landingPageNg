cfeee0be32d2002e0a8ad456e440fa39948b184e
GabiiCarvalho <gabriele.carvalho.16@icloud.com>
2026-03-20 14:17:30 -0300

    correcaoCodeDB

--- config/database.php
+++ config/database.php
@@ -1,5 +1,4 @@
 <?php
-require_once(__DIR__ . '/../../config/database.php');
 header("Access-Control-Allow-Origin: https://ng-express.netlify.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
